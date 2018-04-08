e1e4d6608a12206c58745b08aa5ce0823460b05b
Kate Howard <kate@kate.com>
2018-04-08 22:10:02 +0100

    Adjusted text to make it easier to read

--- trnmnt-gnrtr/src/App.js
+++ trnmnt-gnrtr/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1 className="App-title">Welcome to the <br/>Ping Pong Tournament Generator</h1>
-          <p className="App-intro">Enter your players' names and then generate your tournament structure.<br/>(The number of players must be a power of 2!)</p>
+          <p className="App-intro">Enter your players' names and then generate your tournament structure.<br/>(The number of players must be a power of 2)</p>
         </div>
         <GeneratorContainer />
       </div>
